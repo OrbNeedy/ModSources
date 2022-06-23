9a66d8833a2a13722241e512733eb1e961d51a9f
Reboot <theviod36@gmail.com>
2022-06-23 18:58:59 -0500

    Added an effect for using flashfield, it now damages enemies.

--- gvmod/Common/Players/AdeptPlayer.cs
+++ gvmod/Common/Players/AdeptPlayer.cs
@@ -1,16 +1,18 @@
-using System.Collections.Generic;
 using Terraria;
 using Terraria.GameInput;
 using Terraria.ID;
 using Terraria.ModLoader;
 using gvmod.Common.Systems;
-using System;
+using gvmod.Content.Buffs;
 using Microsoft.Xna.Framework;
+using System.Collections.Generic;
 
 namespace gvmod.Common.Players
 {
     public class AdeptPlayer : ModPlayer
     {
+        private int maxSeptimalPower;
+        private int septimalPower;
         private bool isUsingPrimary;
         public override void ProcessTriggers(TriggersSet triggersSet)
         {
@@ -28,13 +30,38 @@ namespace gvmod.Common.Players
         {
             if (isUsingPrimary)
             {
-                Vector2 pos = new Vector2(64);
+                Vector2 pos = new Vector2(80);
                 for (int i = 0; i < 360; i++)
                 {
                     pos = pos.RotatedBy(MathHelper.ToRadians(1));
-                    if (i%2 == 0) Dust.NewDustDirect(Player.Center + pos, 10, 10, DustID.Electric, 0, 0);
+                    if (i%6 == 0) Dust.NewDustDirect(Player.Center + pos, 10, 10, DustID.MartianSaucerSpark, 0, 0, 0, Color.DeepSkyBlue);
                 }
             }
+        }
+
+        public override void PostUpdate()
+        {
+            List<NPC> closeNPCs = getNPCsInRadius(80);
+            foreach (NPC npc in closeNPCs)
+            {
+                if (!npc.friendly)
+                {
+                    npc.AddBuff(ModContent.BuffType<AzureElectrifiedDebuff>(), 10);
+                }
+            }
+        }
+
+        public List<NPC> getNPCsInRadius(int radius)
+        {
+            var closeNPCs = new List<NPC>();
+            for (int i = 0; i < Main.npc.Length; i++)
+            {
+                var npc = Main.npc[i];
+                if (npc == null) continue;
+                if (Vector2.Distance(npc.Center, Player.Center) > radius) continue;
+                closeNPCs.Add(Main.npc[i]);
+            }
+            return closeNPCs;
         }
     }
 }
---
+++ gvmod/Common/Players/AzureElectrifiedPlayer.cs
@@ -0,0 +1,23 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace gvmod.Common.Players
+{
+    public class AzureElectrifiedPlayer : ModPlayer
+    {
+        public bool azureElectricityDebuff;
+
+        public override void ResetEffects()
+        {
+            azureElectricityDebuff = false;
+        }
+
+        public override void UpdateBadLifeRegen()
+        {
+            if (azureElectricityDebuff)
+            {
+                Player.lifeRegen -= 8;
+            }
+        }
+    }
+}
